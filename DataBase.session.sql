7d6a683be48e1aafad6965adbc5f3c474b209a6e
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-30 10:19:27 +0100

    feat(db): create projects table with owner foreign key

--- DataBase.session.sql
+++ DataBase.session.sql
@@ -9,3 +9,16 @@ CREATE TABLE users (
     role ENUM('admin', 'manager', 'member') NOT NULL DEFAULT 'member',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+--@block
+CREATE TABLE projects (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT,
+    owner_id INT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_projects_owner
+    Foreign Key (owner_id) REFERENCES users(id)
+    ON DELETE CASCADE
+);
