f48da53e61a82b5b0b8ffa8244076c89787024bc
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-12-31 09:54:33 +0100

    feat(db): add simple database connection

---
+++ config/database.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$db   = "athena";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$db;charset=utf8",
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("DB Error: " . $e->getMessage());
+}
