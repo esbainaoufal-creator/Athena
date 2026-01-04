f1a9e217e548a5eb7473149a5ac76f8b2c9c8f85
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-04 13:20:08 +0100

    feat(user): add create() method with secure password hashing

--- user.php
+++ user.php
@@ -2,36 +2,23 @@
 
 <?php
 require_once "config/database.php";
+class User {
+    private $pdo;
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
 
-function createUser($name, $email, $password, $role = "member") {
-    global $pdo;
-
-    $sql = "INSERT INTO users (name, email, password, role) VALUES (:name, :email, :password, :role)";
-
-    $stmt = $pdo->prepare($sql);
-
-    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-
-    return $stmt->execute([
-        ":name" => $name,
-        ":email" => $email,
-        ":password" => $hashedPassword,
-        ":role" => $role
-
-    ]);
-}
-
-function loginUser($email, $password) {
-    global $pdo;
-
-    $sql = "SELECT * FROM users WHERE email = :email LIMIT 1";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([":email" => $email]);
-
-    $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($user && password_verify($password, $user["password"])) {
-        return $user;
     }
-    return false;
+    public function create($name, $email, $password, $role = "member"){
+        $sql = "INSERT INTO users (name, email, password, role) Values (:name, :email, :password, :role)";
+        
+        $stmt = $this->pdo->prepare($sql);
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        return $stmt->execute([
+            ":name" => $name,
+            ":email" => $email,
+            ":password" => $hashedPassword,
+            ":role" => $role
+        ]);
+    }
 }
