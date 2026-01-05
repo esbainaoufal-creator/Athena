007855b920641a4ab11112896aeba545053f7a56
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 14:41:17 +0100

    feat(sprint): add getById() method

--- sprint.php
+++ sprint.php
@@ -37,4 +37,13 @@ public function getByProject($project_id) {
     ]);
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+public function getById($id) {
+    $sql = "SELECT * FROM " . $this->table . "WHERE id = :id";
+    $stmt = $this->conn->prepare($sql);
+    $stmt->execute([
+        ":id" => $id
+    ]);
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
 }
