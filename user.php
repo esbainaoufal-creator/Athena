44535d8b69d9856ff34dfd7cdd4ce96098954955
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 09:23:36 +0100

    feat(user): add User class with constructor and DB connection

--- user.php
+++ user.php
@@ -1,31 +1,21 @@
 <?php
-require_once "config/database.php";
-class User {
-    private $pdo;
-    public function __construct($pdo)
-    {
-        $this->pdo = $pdo;
 
+class User {
+    private $conn;
+    private $table = "users";
+
+    public $id;
+    public $name;
+    public $email;
+    public $password;
+    public $role;
+
+    public function __construct($pdo) {
+        $this->conn = $pdo;
+        if(session_status() === PHP_SESSION_NONE){
+            session_start(); 
+        }
     }
-    public function create($name, $email, $password, $role = "member"){
-        $sql = "INSERT INTO users (name, email, password, role) Values (:name, :email, :password, :role)";
-        
-        $stmt = $this->pdo->prepare($sql);
-        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
-        return $stmt->execute([
-            ":name" => $name,
-            ":email" => $email,
-            ":password" => $hashedPassword,
-            ":role" => $role
-        ]);
-    }
-    public function login($email, $password) {
-        $sql = "SELECT * FROM users WHERE email = :email LIMIT 1";
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute([":email" => $email]);
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($user && password_verify($password, $user["password"])) {
-            return $user;
-        } return false;
-    }
+
+    
 }
