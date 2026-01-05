becc04f72e28541843cee7c0760cd9315a76ef86
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 11:04:35 +0100

    feat(project): add getAll() method to Project class

--- project.php
+++ project.php
@@ -1,7 +1,8 @@
 <?php
 require_once "config/database.php";
 
-class Project {
+class Project
+{
     private $conn;
     private $table = "projects";
 
@@ -10,7 +11,28 @@ class Project {
     public $description;
     public $owner_id;
 
-    public function __construct($pdo) {
+    public function __construct($pdo)
+    {
         $this->conn = $pdo;
+    }
+    public function create()
+    {
+        $sql = "INSERT INTO" . $this->table . "
+        (title, description, owner_id)
+        VALUES (:title, :description, :owner_id)";
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute([
+            ":title" => $this->title,
+            ":description" => $this->description,
+            ":owner_id" => $this->owner_id
+        ]);
+    }
+
+    public function getALL() {
+        $sql = "SELECT * FROM" . $this->table;
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
