e8a7368ef6fcba42eee736d521050aca95860737
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 07:53:49 +0100

    feat(auth): create login.php using User class and sessions

--- login.php
+++ login.php
@@ -1,24 +1,12 @@
 <?php
 session_start();
-?>
-<form method="POST">
-    <label>Email</label><br>
-    <input name="email" placeholder="Email"><br>
-    <label>Password</label><br>
-    <input name="password" type="password" placeholder="Password"><br>
-    <button type="submit">Login</button><br>
-</form>
-
-
-<?php
-
-require_once "user.php";
-
+require_once "../config/database.php";
+require_once "../User.php";
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-
-    $user = loginUser($email, $password);
+    $email = $_POST["email"] ?? "";
+    $password = $_POST["password"] ?? "";
+    $userModel = new User($pdo);
+    $user = $userModel->login($email, $password);
 
     if ($user) {
         $_SESSION["user_id"] = $user["id"];
@@ -26,8 +14,8 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $_SESSION["user_role"] = $user["role"];
 
         echo "Login correcto. Bienvenido " . $user["name"];
-
-    }else {
-        echo "Email o contraseña incorrectos";
-    }
+}else {
+            echo "Email o contraseña incorrectos";
 }
+}
+?>
