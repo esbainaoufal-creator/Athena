1c2943b2d4ce4b56ea16d4594c20ecd373bc22cc
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 11:18:20 +0100

    feat(project): add getById() method to Project class

--- project.php
+++ project.php
@@ -35,4 +35,10 @@ class Project
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+    public function getById($id)  {
+        $sql = "SELECT * FROM" . $this->table . "WHERE id =:id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([":id" => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
