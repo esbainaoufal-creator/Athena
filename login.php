a9696f5f81e45cbb30a398f00e0be9bd48cafc67
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 09:30:12 +0100

    feat(auth): update login.php to use User class for login and session

--- login.php
+++ login.php
@@ -1,21 +1,15 @@
 <?php
-session_start();
 require_once "../config/database.php";
 require_once "../User.php";
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $email = $_POST["email"] ?? "";
     $password = $_POST["password"] ?? "";
+
     $userModel = new User($pdo);
-    $user = $userModel->login($email, $password);
-
-    if ($user) {
-        $_SESSION["user_id"] = $user["id"];
-        $_SESSION["user_name"] = $user["name"];
-        $_SESSION["user_role"] = $user["role"];
-
-        echo "Login correcto. Bienvenido " . $user["name"];
-}else {
-            echo "Email o contraseña incorrectos";
+    if ($userModel->login($email, $password)) {
+        echo "Login correcto. Bienvenido " . $userModel->name;
+    } else {
+        echo "Email o contraseña incorrectos";
+    }
 }
-}
-?>
