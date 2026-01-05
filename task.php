0621e329ef0d3ded8f25743ac6443ad03a21e531
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 14:59:22 +0100

    feat(task): add create() method

--- task.php
+++ task.php
@@ -1,7 +1,8 @@
 <?php
 require_once "config/database.php";
 
-class Task {
+class Task
+{
     private $conn;
     private $table = "tasks";
 
@@ -11,7 +12,23 @@ class Task {
     public $title;
     public $status;
 
-    public function __construct($pdo) {
+    public function __construct($pdo)
+    {
         $this->conn = $pdo;
+    }
+
+    public function create()
+    {
+        $sql = "INSERT INTO " . $this->table . "
+            (sprint_id, user_id, title, status)
+            VALUES (:sprint_id, :user_id, :title, :status)";
+        $stmt = $this->conn->prepare($sql);
+
+        return $stmt->execute([
+            ":sprint_id" => $this->sprint_id,
+            ":user_id"   => $this->user_id,
+            ":title"     => $this->title,
+            ":status"    => $this->status ?? "todo"
+        ]);
     }
 }
