b41d09088b0cbc966013be5e09cb332fb6a42468
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-05 12:14:53 +0100

    feat(sprint): add create() method to Sprint class

---
+++ sprint.php
@@ -0,0 +1,31 @@
+<?php
+require_once "config/database.php";
+
+class Sprint {
+    private $conn;
+    private $table = "sprints";
+
+    public $id;
+    public $project_id;
+    public $name;
+    public $start_date;
+    public $end_date;
+
+    public function __construct($pdo) {
+        $this->conn = $pdo;
+    }
+
+public function create() {
+    $sql = "INSERT INTO " . $this->table . " 
+            (project_id, name, start_date, end_date)
+            VALUES (:project_id, :name, :start_date, :end_date)";
+    $stmt = $this->conn->prepare($sql);
+
+    return $stmt->execute([
+        ":project_id" => $this->project_id,
+        ":name" => $this->name,
+        ":start_date" => $this->start_date,
+        ":end_date" => $this->end_date
+    ]);
+}
+}
