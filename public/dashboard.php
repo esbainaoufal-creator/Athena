24cda39e0a6433924328f2d5cc06396ca0235672
esbainaoufal-creator <esbainaoufal@gmail.com>
2026-01-08 11:02:22 +0100

    feat: display projects on dashboard

--- public/dashboard.php
+++ public/dashboard.php
@@ -1,15 +1,38 @@
-
-
 <?php
 session_start();
 
 if (!isset($_SESSION["user_id"])) {
-    die("Acceso denegado");
+    header("Location: login.php");
+    exit;
 }
 
-echo "Hola " . $_SESSION["user_name"] . "<br>";
-echo "Rol: " . $_SESSION["user_role"];
+require_once "../config/database.php";
+require_once "../models/project.php";
+
+$projectModel = new project($pdo);
+$projects = $projectModel->getAll();
 ?>
 
-<br><br>
-<a href="logout.php">Logout</a>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
+<h2>Bienvenido <?php echo $_SESSION["user_name"]; ?></h2>
+
+<h3>Mis proyectos</h3>
+
+<ul>
+    <?php foreach ($projects as $project): ?>
+        <li>
+            <?php echo htmlspecialchars($project["title"]); ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+<a href="project_form.php">Crear nuevo proyecto</a>
+
+</body>
+</html>
